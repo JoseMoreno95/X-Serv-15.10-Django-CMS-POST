daedfa2094e130f38aaeae396d2b43a01aec3e0b
José Moreno Rodríguez <josemoreno95@outlook.com>
2018-04-23 22:03:56 +0200

    Añadida funcionalidad de POST

---
+++ myproject/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from cms import views
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', views.loginView),
+	url(r'^logout/', views.logoutView),
+    url(r'^authenticate/', views.loginPage),
+    url(r'^$', views.mainPage),
+    url(r'^annotated/(\w+)/$', views.getPageTemplate),
+    url(r'^edit/(\w+)/$', views.editPage),
+    url(r'^(\w+)/$', views.getPage),
+]
